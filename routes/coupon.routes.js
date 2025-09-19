fa53769e16bf1f9f4586b3c79eb2d6c9d959a257
AebadUQ <aebadulquadir123@gmail.com>
2025-09-19 11:40:17 +0500

    fix

--- routes/coupon.routes.js
+++ routes/coupon.routes.js
@@ -4,7 +4,6 @@ const couponController = require("../controllers/coupon.controller");
 const { authMiddleware } = require("../middlewares/auth.middleware");
 router.post("/create", authMiddleware, couponController.createCoupon);
 router.get("/",couponController.getAllCoupons);
-
 router.get("/:id",couponController.getCouponByID);
 router.patch("/:id/use",couponController.updateUses)
 module.exports = router;
