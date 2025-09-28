776b33652b61ad800f330384b9bc2277ebaa3aa9
AebadUQ <aebadulquadir123@gmail.com>
2025-09-29 01:50:36 +0500

    fix

--- app.js
+++ app.js
@@ -9,20 +9,19 @@ const blogRoutes = require("./routes/blog.routes");
 const statsRoutes = require("./routes/stats.routes");
 const faqRoutes = require("./routes/faq.routes");
 const webblogsRoutes = require('./routes/webblog.routes')
-
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
-
-// Allow all origins
-// app.use(cors({
-//   origin: '*',
-//   credentials: true, // note: credentials won't work with '*' in production
-// }));
+const allowedOrigins = ["http://localhost:3001", "https://captaindiscounts.com/"];
 
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use("/api/admin", adminRoutes);
 app.use("/api/admin/category", categoryRoutes);
 app.use("/api/admin/brand", brandRoutes);
@@ -32,10 +31,10 @@ app.use("/api/admin/stats", statsRoutes);
 app.use("/api/admin/faq", faqRoutes);
 app.use("/api/admin/webblog", webblogsRoutes);
 
+
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
-
 app.use(errorHandler);
 
 module.exports = app;
