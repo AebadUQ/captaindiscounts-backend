7faf5c0017b4490a085afcfd7a0f8d68b982138b
AebadUQ <aebadulquadir123@gmail.com>
2025-09-29 02:46:00 +0500

    co

--- app.js
+++ app.js
@@ -12,15 +12,26 @@ const webblogsRoutes = require('./routes/webblog.routes')
 const cors = require("cors");
 
 const app = express();
-const allowedOrigins = ["http://localhost:3001", "https://captaindiscounts.com/"];
+// const allowedOrigins = ["http://localhost:3001", "https://captaindiscounts.com/"];
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   })
+// );
+const allowedOrigins = ["https://captaindiscounts.com"];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.includes(origin)) callback(null, true);
+    else callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+
 app.use("/api/admin", adminRoutes);
 app.use("/api/admin/category", categoryRoutes);
 app.use("/api/admin/brand", brandRoutes);
