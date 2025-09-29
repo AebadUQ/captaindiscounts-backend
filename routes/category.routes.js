b69e6ecbaed82d92833b5f6da6d7d0f649e655f1
AebadUQ <aebadulquadir123@gmail.com>
2025-09-29 10:44:30 +0500

    fix

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -4,7 +4,7 @@ const categoryController = require("../controllers/category.controller");
 const { authMiddleware } = require("../middlewares/auth.middleware");
 router.post("/create", authMiddleware, categoryController.createCategory);
 router.get("/category-with-brands", categoryController.getCategoryWithBrands);
-router.get("/", authMiddleware, categoryController.getAllCategories);
+router.get("/", categoryController.getAllCategories);
 router.get("/:id", authMiddleware, categoryController.getCategoryByID);
 router.delete("/:id", authMiddleware, categoryController.deleteCategory);
 router.put("/:id", authMiddleware, categoryController.updateCategory);
